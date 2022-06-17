7ed9932d3179c607eb244a20c6f2a113fbdd9015
Yannick Spoerl <yannickspoerl@protonmail.com>
2022-06-17 16:28:48 +0200

    Change url

--- quasar.config.js
+++ quasar.config.js
@@ -55,7 +55,7 @@ module.exports = configure(function (ctx) {
       vueRouterMode: 'history', // available values: 'hash', 'history'
 
       // transpile: false,
-      publicPath: '/browse-tv',
+      publicPath: '/yannickflix',
 
       // Add dependencies for transpiling with Babel (Array of string/regex)
       // (from node_modules, which are by default not transpiled).
