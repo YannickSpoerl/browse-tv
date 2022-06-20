e3505a1f3231fa0e88cdfdf235b44d7685db6c0a
Yannick Spoerl <yannickspoerl@protonmail.com>
2022-06-20 17:02:04 +0200

    Logo clickable

--- src/components/LogoImg.vue
+++ src/components/LogoImg.vue
@@ -1,9 +1,14 @@
 <template>
-  <img
-    src="../assets/logo.png"
-    alt="Yannickflix logo"
+  <a
+    href="https://yannickspoerl.de/browse-tv"
     style="max-height: inherit; max-width: inherit"
-  />
+  >
+    <img
+      src="../assets/logo.png"
+      alt="Yannickflix logo"
+      style="max-height: inherit; max-width: inherit"
+    />
+  </a>
 </template>
 <script>
 import { defineComponent } from 'vue';
